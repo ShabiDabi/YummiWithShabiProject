8d0ab497f60127e82f871ce92c6a8bcbad8fea56
Mor <shabi1mor@gmail.com>
2018-08-05 21:26:25 +0300

    with toolbar

--- app/src/main/java/com/example/mor/yammiwithshabi/HomePageActivity.java
+++ app/src/main/java/com/example/mor/yammiwithshabi/HomePageActivity.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -16,9 +22,35 @@ public class HomePageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SetupDrawer(toolbar);
+
+
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.nameView);
         textView.setText("Welcome " + user.getEmail());
+    }
+
+    private void SetupDrawer(Toolbar toolbar) {
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_Feed);
+        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(3).withName(R.string.drawer_item_logOut);
+
+
+        Drawer drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        //new DividerDrawerItem(),
+                        item2,
+                        //new SecondaryDrawerItem().withName(R.string.drawer_item_settings),
+                        item3
+                        //new SecondaryDrawerItem().withName(R.string.drawer_item_logOut)
+                )
+                .build();
     }
 }
